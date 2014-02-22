307444bb137cd950cb9d786d58d4d10b2e0f2d19
Rich Adams <git@richadams.me>
2014-02-22 17:34:14 +0900

    Adding spotofy playlist

--- www/js/objects/playlist.js
+++ www/js/objects/playlist.js
@@ -23,14 +23,33 @@ function Playlist(data)
         for (var tid in self.tracks)
         {
             var track = self.tracks[tid];
-            console.log(track);
             $("#playlist").append(
                 "<li class=\"track\">"
                 + track.getArtist() + " - " + track.getTitle()
                 + "</li>"
             );
         }
+
+        // TODO: We might not have all the spotify results yet!!!
+        // TODO: This is a massive hack.
+        setTimeout(function() { self.renderSpotify(); }, 5000);
     };
+
+    // Render spotify playlist
+    self.renderSpotify = function()
+    {
+        $("#spotify").html(
+            "<iframe src=\"" + self.generateSpotifyURI() + "\" frameborder=\"0\" width=\"300\" height=\"700\" allowtransparency=\"true\"></iframe>"
+        );
+    }
+
+    // Generate the spotify playlist URI
+    self.generateSpotifyURI = function()
+    {
+        var url = "https://embed.spotify.com/?uri=spotify:trackset:パチラジオ:";
+        for (var i in self.tracks) { url = url + self.tracks[i].getSpotifyID() + ","; }
+        return url.substring(0, url.length - 1); // Remove final ","
+    }
 
     self._construct(data);
 }
--- www/js/objects/track.js
+++ www/js/objects/track.js
@@ -4,20 +4,54 @@ function Track(data)
     var self   = this; // Self-reference
 
     // Only care about these properties for now.
-    self.hash  = null;
-    self.data  = null;
+    self.hash      = null;
+    self.data      = null;
+    self.spotifyID = null;
 
     // Constructor
     self._construct = function _construct(data)
     {
         self.data = data;
         self.hash = $.md5(self.data.TITLE[0].VALUE + "##" + self.data.ARTIST[0].VALUE);
+
+        // Try to acquire the spotify ID.
+        self.lookupSpotifyIdentifier();
     }
 
     // Getters
-    self.getTitle  = function() { return self.data.TITLE[0].VALUE; }
-    self.getArtist = function() { return self.data.ARTIST[0].VALUE; }
-    self.getHash   = function() { return self.hash; }
+    self.getTitle     = function() { return self.data.TITLE[0].VALUE; }
+    self.getArtist    = function() { return self.data.ARTIST[0].VALUE; }
+    self.getSpotifyID = function() { return self.spotifyID; }
+    self.getHash      = function() { return self.hash; }
+
+    // Make a best effort to get the Spotify ID.
+    self.lookupSpotifyIdentifier = function()
+    {
+        var url = "https://ws.spotify.com/search/1/track.json?q=" + self.getArtist() + " " + self.getTitle();
+        jQuery.ajax(
+        {
+            async: true,
+            type: "GET",
+            url: url,
+            data: null,
+            success: function(data, textStatus, jqxhr)
+            {
+                // If we got some tracks back
+                if (data.tracks.length > 0)
+                {
+                    // Assume first track is best match.. for now.
+                    self.spotifyID = data.tracks[0].href.replace("spotify:track:", "");
+                    console.log("[track] spotify ID = " + self.spotifyID);
+                }
+            },
+            error: function (xhr, textStatus, errorThrown)
+            {
+                // Throw to Chrome console, jQuery won't alert as it will catch it itself.
+                console.error("Error from remote server " + url);
+                console.error(errorThrown);
+            }
+        });
+    }
 
     // Need to account for no album art, as server will not return the data, grr.
     self.getArt = function()
