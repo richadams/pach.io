cb712a727a348d485e3769e3d458bc453449bafe
Rich Adams <git@richadams.me>
2014-02-23 13:35:10 +0900

    updating eras

--- www/js/lookups/eras.js
+++ www/js/lookups/eras.js
@@ -3,8 +3,8 @@ var _eras = new Array(
 {id:29483,text:"2000 年代",texten:"2000's"},
 {id:29484,text:"1990 年代",texten:"1990's"},
 {id:29485,text:"1980 年代",texten:"1980's"},
-{id:29486,text:"1970 年代",texten:"1970's"}/*,
-{id:29487,text:"1960 年代",texten:"1960's"},
+{id:29486,text:"1970 年代",texten:"1970's"},
+{id:29487,text:"1960 年代",texten:"1960's"}/*,
 {id:29488,text:"1950 年代",texten:"1950's"},
 {id:29489,text:"1940 年代",texten:"1940's"},
 {id:29490,text:"1930 年代",texten:"1930's"}*/
