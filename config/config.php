a9b77ee9058c930266ca32a9a4855185664b6fae
Rich Adams <git@richadams.me>
2014-03-01 13:04:34 -0800

    Removing private info from git history, credentials, etc.

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+// Gracenote API configuration, you can get an account at https://developer.gracenote.com/
+$_CONFIG['gn_client_id'] = "****";
+$_CONFIG['gn_client']    = "****";
+$_CONFIG['gn_user_id']   = "****";
+
+// Pach.io configuration
+$_CONFIG['pachio_result_count'] = 10;
